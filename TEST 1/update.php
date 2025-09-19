eae902dd3683a4a72a077201ddf54f8b235d93a7
dianah-arch <dianahkyoshabire71@gmail.com>
2025-09-19 21:03:12 +0300

    Second commit

--- TEST 1/update.php
+++ TEST 1/update.php
@@ -17,7 +17,7 @@ if (isset($_GET['id'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Staff Management System</title>
 </head>
 <body>
     <h2>Edit Staff</h2>
